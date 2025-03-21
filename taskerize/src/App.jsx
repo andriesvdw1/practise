9e16c48f8a20c37c7173a7bea711ab4040aa4f1e
andriesvdw1 <andriesvdw1@gmail.com>
2025-03-21 23:29:08 +0200

    Added a ToDo task list feature with unique keys for list items.

--- taskerize/src/App.jsx
+++ taskerize/src/App.jsx
@@ -5,6 +5,11 @@ import Footer from './components/Footer';
 import { useState } from 'react';
 function App() {
   const [count, setcount] = useState(0);
+  const [tasks, setTasks] = useState([{id: 1, name:"learn components", completed: true},
+    {id: 2, name:"learn about dynamic content", completed: true},
+    {id: 3, name:"learn about state", completed: true},
+    {id: 4, name:"learn about other React hooks", completed: false}  
+  ]);
   function handleAdd(){
     setcount(count + 1);
   }
@@ -27,7 +32,15 @@ function App() {
         </div>
       </div>
       <p className='active'>{username}</p>
-
+      <h1>Task List ToDo:</h1>
+      <ul>
+        { tasks.map(({id, name, completed}) => (
+          <li key={id}>
+            <span>{id} - {name}</span>
+            <button>Remove task</button>
+          </li>
+        )) }
+      </ul>
       <Footer/>
     </React.Fragment>
   )
