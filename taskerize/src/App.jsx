4929dc409309d13d1c387b9c837a5fe8942fe2d5
andriesvdw1 <andriesvdw1@gmail.com>
2025-03-21 22:19:02 +0200

    Add Header and Footer component

--- taskerize/src/App.jsx
+++ taskerize/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react'
-
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   const username= "Andries";
   return (
     <React.Fragment>
-    <div>App</div>
-    <p className='active'>{username}</p>
-
+      <Header/>
+      <div>App</div>
+      <p className='active'>{username}</p>
+      <Footer/>
     </React.Fragment>
   )
 }
