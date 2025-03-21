bb9bcb26cfa063cfb050f68ab901c38df2e1a6e5
andriesvdw1 <andriesvdw1@gmail.com>
2025-03-21 23:07:04 +0200

    Add a counter with useState hook. Also add subtract and reset buttons that change the value of the counter on the screen.

--- taskerize/src/App.jsx
+++ taskerize/src/App.jsx
@@ -2,13 +2,32 @@ import './App.css';
 import React from 'react'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { useState } from 'react';
 function App() {
+  const [count, setcount] = useState(0);
+  function handleAdd(){
+    setcount(count + 1);
+  }
+  function handleSub(){
+    setcount(count - 1);
+  }
+  function handleReset(){
+    setcount(0);
+  }
   const username= "Andries";
   return (
     <React.Fragment>
       <Header/>
-      <div>App</div>
+      <div className='App'>
+        <div className="box">
+          <p>The current count is: {count}</p>
+          <button onClick={handleAdd} className='add'>+</button>
+          <button onClick={handleReset} className="reset">Reset</button>
+          <button onClick={handleSub}className="subtract">-</button>
+        </div>
+      </div>
       <p className='active'>{username}</p>
+
       <Footer/>
     </React.Fragment>
   )
--- taskerize/src/components/Header.jsx
+++ taskerize/src/components/Header.jsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo.gif"
 import './header.css';
 function Header() {
   return (
-    <img className="logo" src={Logo} alt="picture of logo" />
+    <img className="logo" src={Logo} alt="logo" />
     
   )
 }
