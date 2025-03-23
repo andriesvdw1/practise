7b3864f087877ca59f3c82508f6efa77d9e2e286
andriesvdw1 <andriesvdw1@gmail.com>
2025-03-23 19:25:49 +0200

    A task can be generated with a random id assigned to it, and the name of the task as provided by the user in the input field. The new task now shows in the console only. Not on the form yet.

--- taskerize/src/components/AddTask.jsx
+++ taskerize/src/components/AddTask.jsx
@@ -1,20 +1,40 @@
 import { useState } from "react";
+
 function AddTask() {
-    const [taskValue, setTaskValue] = useState("");
-    const handleChange = (event) => {
-        setTaskValue(event.target.value);
-    }
+  const [taskValue, setTaskValue] = useState("");
+  const handleChange = (event) => {
+    setTaskValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    let task = {
+      id: Math.floor(Math.random() * 10000),
+      name: event.target.task.value, 
+      completed: false,
+    };
+    console.log(task);
+  };
+
   return (
     <section className="add-task">
-        <form>
-            <h1>Add a new task:</h1>
-            <label htmlFor="task">Task Name:</label>
-            <input onClick={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" />
-            <input style={{backgroundColor:"yellow"}} type="submit" value="Add task" />
-            <p>{taskValue}</p>
-        </form>
+      <form onSubmit={handleSubmit}> // Pass the handleSubmit function to the form
+        <h1>Add a new task:</h1>
+        <label htmlFor="task">Task Name:</label>
+        <input
+          type="text"
+          name="task"
+          id="task"
+          placeholder="Task Name"
+          autoComplete="off"
+          value={taskValue} 
+          onChange={handleChange}
+        />
+        <input style={{ backgroundColor: "yellow" }} type="submit" value="Add task" />
+        <p>{taskValue}</p>
+      </form>
     </section>
-  )
+  );
 }
 
-export default AddTask
+export default AddTask;
--- taskerize/src/components/TaskCard.jsx
+++ taskerize/src/components/TaskCard.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 import '../index.css';
 import './TaskCard.css';
-
 function TaskCard({ task, handleDelete }) {
   return (
     <table style={{borderWidth: "thin", border: "black", borderStyle:"solid"}}>
-      <tr>
-      <th>Task</th>
-      <th>Action</th>      
-      </tr>
-      
-      <tr>
-        <td width={400}>
-          <span className="task-card-details">
-            {task.id} - {task.name}
-          </span>
-        </td>
-        
-        <td className="task-card-button-col">
-          <button
-            className="task-card-button"
-            onClick={() => handleDelete(task.id)}
-          >Remove task
-          </button>
-        </td>
-      </tr>
+      <thead>
+        <tr>
+          <th>Task</th>
+          <th>Action</th>      
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td style={{width: "400px"}}>
+            <span className="task-card-details">
+              {task.id} - {task.name}
+            </span>
+          </td>
+          <td className="task-card-button-col">
+            <button
+              className="task-card-button"
+              onClick={() => handleDelete(task.id)}
+            >Remove task
+            </button>
+          </td>
+        </tr>
+      </tbody>
     </table>
   );
 }
 
-export default TaskCard;
+export default TaskCard
